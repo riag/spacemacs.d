fdb82e5b4203124e2991eb3c3f391b96686f95af
riag <riag@163.com>
2016-10-22 16:00:20 +0800

    增加 keyfreq layer

--- init.el
+++ init.el
@@ -89,7 +89,7 @@ values."
 
      ;; 自定义的layers
      which-function
-     ;;keyfreq
+     keyfreq
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
